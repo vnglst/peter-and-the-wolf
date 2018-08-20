a67c9054ade5e05f585b8dd0bd5201df9dbf80f1
Koen van Gilst <koen.vangilst@gmail.com>
2018-08-20 21:40:52 +0200

    add prod check for serviceworker

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-if ('serviceWorker' in navigator) {
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js');
 }
 

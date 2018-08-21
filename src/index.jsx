f4968273e3dcf170b747a5da83c2f9cda194359e
Koen van Gilst <koen.vangilst@gmail.com>
2018-08-21 21:36:56 +0200

    cache fonts, better logging of sw loading

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
+/* eslint no-console: 0 */
+
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js');
+  console.log('Service Worker App loading started');
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => {
+      console.log('Service Worker App Registered');
+    })
+    .catch(() => {
+      console.error('Error registering Service Worker');
+    });
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,15 +1,17 @@
 module.exports = {
   staticFileGlobs: [
-    'build/css/**.css',
+    'build/css/**.*',
     'build/**.html',
+    'build/**.json',
     'build/images/**.*',
+    'build/sounds/**.mp3',
     'build/js/**.*',
   ],
   stripPrefix: 'build/',
   maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
   runtimeCaching: [
     {
-      urlPattern: /\/sounds\//,
+      urlPattern: /https?:\/\/fonts.+/,
       handler: 'cacheFirst',
     },
   ],

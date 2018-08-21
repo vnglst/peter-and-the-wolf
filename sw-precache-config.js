530ec0fefdc61e1d4b85af186cbec3b54be90d89
Koen van Gilst <koen.vangilst@gmail.com>
2018-08-21 17:29:24 +0200

    cache sounds during runtime

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,9 +3,14 @@ module.exports = {
     'build/css/**.css',
     'build/**.html',
     'build/images/**.*',
-    'build/sounds/**.*',
     'build/js/**.*',
   ],
   stripPrefix: 'build/',
   maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
+  runtimeCaching: [
+    {
+      urlPattern: /\/sounds\//,
+      handler: 'cacheFirst',
+    },
+  ],
 };

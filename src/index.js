0bdd28444bf9d49d6e84f899dfb172ee4a7bab3b
Koen van Gilst <koen.vangilst@gmail.com>
2018-08-23 13:59:02 +0200

    just use js, no more jsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './App';
 
 /* eslint no-console: 0 */
 
--- webpack/paths.js
+++ webpack/paths.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   root: path.resolve(__dirname, '../'),
   outputPath: path.resolve(__dirname, '../', 'build'),
-  entryPath: path.resolve(__dirname, '../', 'src/index.jsx'),
+  entryPath: path.resolve(__dirname, '../', 'src/index.js'),
   templatePath: path.resolve(__dirname, '../', 'src/template.html'),
   imagesFolder: 'images',
   soundsFolder: 'sounds',

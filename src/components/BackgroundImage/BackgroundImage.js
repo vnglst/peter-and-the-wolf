136c98bc4ca0f42fe7988a10070ef18991400479
Koen van Gilst <koen.vangilst@gmail.com>
2018-09-11 12:07:38 +0200

    remove classNames as a depency

--- src/components/BackgroundImage/BackgroundImage.js
+++ src/components/BackgroundImage/BackgroundImage.js
@@ -1,16 +1,19 @@
-import cx from 'classnames';
 import * as React from 'react';
 import style from './BackgroundImage.css';
 
-const BackgroundImage = ({ className, imageSrc, children, ...other }) => (
-  <div>
-    <div
-      className={cx(style['background-image'], className)}
-      style={{ backgroundImage: `url('${imageSrc}')` }}
-      {...other}
-    />
-    <div className={style['background-image-children']}>{children}</div>
-  </div>
-);
+const BackgroundImage = ({ className, imageSrc, children, ...other }) => {
+  let cx = style['background-image'];
+  cx += className ? ' ' + className : '';
+  return (
+    <div>
+      <div
+        className={cx}
+        style={{ backgroundImage: `url('${imageSrc}')` }}
+        {...other}
+      />
+      <div className={style['background-image-children']}>{children}</div>
+    </div>
+  );
+};
 
 export default BackgroundImage;
